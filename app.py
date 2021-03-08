d0225e516234229d66accdadc71a49dfaff5b303
cornliu <cornliu@gmail.com>
2021-03-08 15:26:11 +0800

    'test'

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = event.source.user_Id
+    message = event.source.user_id
     print(message)
     print(type(message))
     print(str(message))
