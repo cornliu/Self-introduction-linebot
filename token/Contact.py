baf0be62fd34fc759cc10b51d7614d08a730ef21
cornliu <cornliu@gmail.com>
2021-03-09 03:09:35 +0800

    'contact'

--- token/Contact.py
+++
@@ -1,3 +0,0 @@
-import json
-def contact_block():
-    FlexMessage = json.load(open('../Contact.json','r',encoding='utf-8'))
--- utils.py
+++
@@ -1,7 +0,0 @@
-from app import users
-from User import *
-def add_user(user_id):
-    global users
-    if user_id not in users.keys():
-        users[user_id] = User(user_id)
-    return
