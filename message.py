8ef6b561a96d28c92c70edd3b92f07fa75f5cec4
cornliu <cornliu@gmail.com>
2021-03-10 10:26:58 +0800

    'cool'

--- message.py
+++ message.py
@@ -30,8 +30,8 @@ def text_msg(user, msg):
     elif msg.lower() == 'skills':
         user.state = 4
         image_message = ImageSendMessage(
-            original_content_url='https://imgur.com/8UzKglJ.jpg',
-            preview_image_url='https://imgur.com/8UzKglJ.jpg'
+            original_content_url='https://i.imgur.com/qmzi3q8.jpg',
+            preview_image_url='https://i.imgur.com/qmzi3q8.jpg'
         )
         return image_message
     elif msg.lower() == 'contact':
